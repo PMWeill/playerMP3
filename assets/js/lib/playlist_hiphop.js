34127a26ebf65b4868d9902e29f4fc0a21da5531
PMWeill <pierre-manuel.weill@laposte.net>
2023-07-12 16:58:02 +0200

    ajout jevascript

---
+++ assets/js/lib/playlist_hiphop.js
@@ -0,0 +1,44 @@
+const playlist_hiphop = [
+    {
+        "title": "sfqsgfsgdsfg",
+        "author": "skjdsgois",
+        "cover": "./assets/img/cover/macover.jpg",
+        "year": 1971,
+        "audio": "./assets/img/audio/blabla.mp3",
+        "genre": ['HIPHOP', 'JAZZ'],
+        "description": "lorem"
+
+    },
+    {
+        "title": "sfqsgfsgdsfg",
+        "author": "skjdsgois",
+        "cover": "./assets/img/cover/macover.jpg",
+        "year": 1971,
+        "audio": "./assets/img/audio/blabla.mp3",
+        "genre": ['HIPHOP', 'JAZZ'],
+        "description": "lorem"
+
+    },
+    {
+        "title": "sfqsgfsgdsfg",
+        "author": "skjdsgois",
+        "cover": "./assets/img/cover/macover.jpg",
+        "year": 1971,
+        "audio": "./assets/img/audio/blabla.mp3",
+        "genre": ['HIPHOP', 'JAZZ'],
+        "description": "lorem"
+
+    },
+    {
+        "title": "sfqsgfsgdsfg",
+        "author": "skjdsgois",
+        "cover": "./assets/img/cover/macover.jpg",
+        "year": 1971,
+        "audio": "./assets/img/audio/blabla.mp3",
+        "genre": ['HIPHOP', 'JAZZ'],
+        "description": "lorem"
+
+    },
+    //demain il faudra rajouter des listes
+]
+export {playlist_hiphop/* autre variable */}
---
+++ assets/js/script.js
@@ -0,0 +1,4 @@
+//lorsque j'importe un ou plusieurs éléments je le fait tjrs en début de script
+import { playlist_hiphop } from "./lib/playlist_hiphop.js";
+console.dir(playlist_hiphop);
+console.log("Hello");
