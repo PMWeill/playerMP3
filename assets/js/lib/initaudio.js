560a14b04847795513b15aeee8b34542d228345c
PMWeill <pierre-manuel.weill@laposte.net>
2023-07-19 16:51:45 +0200

    finalisation de fonction audio javascript

--- assets/js/lib/initaudio.js
+++ assets/js/lib/initaudio.js
@@ -7,12 +7,35 @@ const initAudio = (playlist) => {
     }
   }
   function backCurrentTrack() {
-    if (currentTrack < playlist.length - 1) {
-      currentTrack--;
+    if (currentTrack === 0) {
+      currentTrack = playlist.length - 1;
+      
     } else {
-      currentTrack = 0;
+      
+      currentTrack--;
     }
   }
+//function de formatage de temps audio
+const horloge = (temps) => {
+
+
+  let totalHeures = Math.floor(temps/3600);
+
+  let totalMinutes = Math.floor(temps/60);
+  // pour récupérer le nombre de secondes restantes à mon temps exprimé en minutes j'utilise l'opérateur mathématique modulo (%)
+  let totalSeconde = Math.floor(temps % 60);
+  console.dir(totalHeures);
+  console.dir(totalMinutes);
+  console.log(totalSeconde);
+  // si j'utilise à la suite d'une conditions qu'une seule insctruction, je poeu la déclarer  sans les {}.
+if (totalHeures<=9)totalHeures = "0"+totalHeures;
+if (totalMinutes<=9)totalMinutes = "0"+totalMinutes;
+if (totalSeconde<=9)totalSeconde = "0"+totalSeconde;
+//ls fonctions peuvent retourner le resultat d'une opération avec le mot clè return
+return(totalHeures+":"+totalMinutes+":"+totalSeconde);
+}
+
+
   //autre type de déclaration de fonction avec le mots clé function
   const togglePlayButton = () => {
     document.querySelector("#playPause i").classList.toggle("fa-play");
@@ -22,8 +45,12 @@ const initAudio = (playlist) => {
   const playPause = document.querySelector("#playPause");
   const next = document.querySelector(".next");
   const back = document.querySelector(".back");
+  const plus = document.querySelector(".plus");
+  const moin = document.querySelector(".moin");
+  const time = document.querySelector(".time");
   const audio = new Audio(playlist[currentTrack].audio);
   audio.isPlaying = false;
+  audio.volume = .5;
   console.dir(audio);
 
   playPause.addEventListener("click", () => {
@@ -73,12 +100,14 @@ const initAudio = (playlist) => {
       togglePlayButton();
     }
   });
+
   back.addEventListener("click", () => {
+    console.log(currentTrack);
     //lecture ou pas
     //si lecture (isPlaying = true)
     if (audio.isPlaying) {
       audio.isPlaying = true;
-      console.log(currentTrack);
+
       // -> audio.pause(),
       audio.pause();
       //-> currentTrack++avec condition de taille du tableau d'objet,
@@ -104,6 +133,82 @@ const initAudio = (playlist) => {
       togglePlayButton();
     }
   });
+
+
+  moin.addEventListener("click", () => {
+    console.dir(audio.volume);
+    // je veux retirer 10% au volume actuelle (entre 0 et 1 fixé a 1 par défaut)
+    
+    audio.volume = Math.round(audio.volume*100)/100;
+    if (audio.volume >0){
+      audio.volume = audio.volume - 0.1;
+      // ou audio.volume -= 0.1; (version raccourcie)
+    }
+    
+    
+
+  });
+
+
+  plus.addEventListener("click", () => {
+    console.dir(audio.volume);
+    // je veux retirer 10% au volume actuelle (entre 0 et 1 fixé a 1 par défaut)
+    
+    audio.volume = Math.round(audio.volume*100)/100;
+    if (audio.volume <1){
+      audio.volume = audio.volume + 0.1;
+      // ou audio.volume += 0.1; (version raccourcie)
+    }
+  });
+setTimeout(() => {
+  //grace au return de ma fonction horlaoge je peux utiliser horloge comme une valeur
+  let tmpHorloge = horloge(audio.duration)
+ console.log(tmpHorloge); 
+setInterval(() => {
+  console.log(horloge(Math.round(audio.currentTime)));
+  time.textContent = horloge(Math.round(audio.currentTime))+"/"+horloge(audio.duration)
+}, 1000);
+
+}, 200);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 };
 
 export { initAudio };
